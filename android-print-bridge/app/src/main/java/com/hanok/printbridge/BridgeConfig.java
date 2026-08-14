8c93b76ca7eef77789f35a8312230cbc6538e39f
GreatDaniel93 <danielchen993@gmail.com>
2026-08-14 17:49:50 +1000

    Add embedded Wagga bridge configuration

---
+++ android-print-bridge/app/src/main/java/com/hanok/printbridge/BridgeConfig.java
@@ -0,0 +1,11 @@
+package com.hanok.printbridge;
+
+public final class BridgeConfig {
+    private BridgeConfig(){}
+    public static final String PREFS="bridge";
+    public static final String BASE_URL="https://orderhanokbbqwagga.com";
+    public static final String DEFAULT_TOTAL_IP="192.168.8.231";
+    public static final String DEFAULT_SPLIT_IP="192.168.8.232";
+    public static final int DEFAULT_PORT=9100;
+    public static final String PRINT_SECRET="ADJH9vefJBk-3wooEAN_0il_o0EKA0ib14hSjbxVYqk";
+}
