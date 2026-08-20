435e2aacca1333293947f951d8481853ce54bc9d
GreatDaniel93 <danielchen993@gmail.com>
2026-08-21 00:34:45 +1000

    Add universal home navigation to internal pages

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css';
+import SystemNav from './components/SystemNav';
 
 export const metadata = {
   title: 'Hanok Unlimited Ordering',
@@ -6,5 +7,5 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
+  return <html lang="en"><body><SystemNav/>{children}</body></html>;
 }
