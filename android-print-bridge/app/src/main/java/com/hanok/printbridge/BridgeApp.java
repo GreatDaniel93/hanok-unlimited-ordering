f6c01772fdf3d48d8fa47fde05746cd3e6446bee
GreatDaniel93 <danielchen993@gmail.com>
2026-08-22 14:52:39 +1000

    Arm always-on watchdog at process start

--- android-print-bridge/app/src/main/java/com/hanok/printbridge/BridgeApp.java
+++ android-print-bridge/app/src/main/java/com/hanok/printbridge/BridgeApp.java
@@ -1,10 +1,14 @@
 package com.hanok.printbridge;
 
 import android.app.Application;
+import android.content.SharedPreferences;
 
 public class BridgeApp extends Application {
     @Override public void onCreate(){
         super.onCreate();
+        SharedPreferences p=getSharedPreferences(BridgeConfig.PREFS,MODE_PRIVATE);
+        p.edit().putInt("process_start_count",p.getInt("process_start_count",0)+1).putLong("process_started_at",System.currentTimeMillis()).apply();
         PrinterHealthMonitor.start(this);
+        if(p.getBoolean("enabled",false))BridgeWatchdog.forceArm(this);
     }
 }
