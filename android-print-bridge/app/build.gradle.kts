697aa4445a155f0571d4e5a9b00f63ca8d88bd56
GreatDaniel93 <danielchen993@gmail.com>
2026-08-22 14:15:11 +1000

    Bump print bridge to v1.7

--- android-print-bridge/app/build.gradle.kts
+++ android-print-bridge/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "com.hanok.printbridge"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.6"
+        versionCode = 7
+        versionName = "1.7"
     }
     buildTypes { release { isMinifyEnabled = false } }
 }
